24f511c8cd050dc4760edf6ffda6abdba807dacf
OliciaMeynard <olicia.meynard@gmail.com>
2023-05-28 23:38:44 +0800

    some changes

--- logininmodal.js
+++ logininmodal.js
@@ -10,15 +10,16 @@
 
     const markup = `
     <section class="login-in-modal">
-    <div class="inner-section-div">
+    <div class="inner-section-div"> 
       <div class="overlay-log-in"> </div>
+ 
        <form action="" class="login-modal-form ">
         <h2 class="loginheader">
           Login
         </h2>
   
         <div class="email-div">
-         <label  for="pickup">Email</label>
+         <label class="universal-label" for="pickup">Email</label>
           <div class="div-for-input ">
            <input class="universal-input" type="email" name="email" id="email-login">
   
@@ -26,7 +27,7 @@
         </div>
   
         <div class="password-div">
-         <label  for="pickup">Password</label>
+         <label  class="universal-label" for="pickup">Password</label>
           <div class="div-for-input ">
            <input class="universal-input" type="password" name="password" id="password-login">
   
@@ -36,7 +37,7 @@
          
          <button class="btn-view-details">Login</button>
          <div class="div-close-btn-login">
-           <i class="fa-solid fa-square-xmark close-btn-modal-order"></i>
+           <i class="fa-solid fa-circle-xmark close-btn-modal-order"></i>
          </div>
          <a href="signup.html" class="not-yet-registered-link">Sign Up instead</a>
         </form>
@@ -53,7 +54,24 @@ return body.insertAdjacentHTML('beforeend', markup)
 body.addEventListener('click', e =>{
 
     const clicked = e.target.closest('.div-close-btn-login')
-    const overlayClicked = e.target.closest('.overlay-log-in')
+    if(!clicked) return
+    if(clicked){
+        const logInModal = document.querySelector('.login-in-modal')
+        logInModal.remove()
+      }
+      
+      if(overlayClicked){
+      const logInModal = document.querySelector('.login-in-modal')
+      logInModal.remove()
+
+    }
+
+})
+
+body.addEventListener('click', e =>{
+
+
+    const clicked = e.target.closest('.overlay-log-in')
     if(!clicked) return
     if(clicked){
         const logInModal = document.querySelector('.login-in-modal')
